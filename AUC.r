4d6cabcfbc9744353fe382d15e23e4451102ba9d
lgxqzz <zhouzhang0418@gmail.com>
2020-01-28 14:45:21 -0600

    modify

--- AUC.r
+++ AUC.r
@@ -1,14 +1,18 @@
-# format AUC to % and add 95% CI
-format_number <- function(x){
+### AUC figures and 
+# out_train_4 <- out_function("GI", "HEA", info.heat, score="frag_mean", "Cat.2")
+
+#format AUC to % and add 95% CI
+format_number <- function(x)
+{
   x1 <- format(round(x[1], 1), nsmall = 1)
   x2 <- format(round(x[2], 1), nsmall = 1)
   x3 <- format(round(x[3], 1), nsmall = 1)
   paste0(x2, "% (95% CI, ", x1, "-", x3, ")")
-  
 }
 
 # calcuate auc and save specificity, sensitivity, AUC, etc.
-out_function <- function(obj, ref, data, score,  Diagnosis_column){
+out_function <- function(obj, ref, data, score,  Diagnosis_column)
+{
   data <- data[data[, Diagnosis_column] %in% c(obj, ref), ]
   data[, Diagnosis_column] <- factor(data[, Diagnosis_column], levels = c(ref, obj))
   out_train <- data.frame(score = data[, score], True_respose = data[, Diagnosis_column])
@@ -24,7 +28,8 @@ out_function <- function(obj, ref, data, score,  Diagnosis_column){
 }
 
 # roc function to return roc object and plot auc.
-roc_function <- function(data, print.auc=T, name, main){
+roc_function <- function(data, print.auc=T, name, main)
+{
   rocobj <- plot.roc(data[,2], data[,1], # data[, 2] is true_respose, data[, 1] is the score
                      cex.main=2,
                      cex.lab=2,
